c091c19d913c2570475aa03d623efb549d37f5ca
ivanramos242 <ivanramos242@gmail.com>
2026-06-30 18:37:07 +0200

    fix legacy test redirect

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
   async redirects() {
     return [
       {
-        source: "/test-online/",
+        source: "/test-online",
         destination: "/test-competencia-profesional-mercancias/",
         permanent: true,
       },
